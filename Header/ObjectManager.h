1c7b398c48cb08c0cfe51ddb895a0dad18b0bfde
CappLucas <lucascapp977@gmail.com>
2024-11-04 15:34:51 -0700

    Wrote suedo code

--- Header/ObjectManager.h
+++ Header/ObjectManager.h
@@ -10,6 +10,113 @@
 #include <string>
 #include <algorithm>
 
+/*------------------------class Structure ------------------
+
+We have a Object Manager that contains the game objects.
+
+We give the object manager an instance of collision manager that has a reference to all objects.
+
+Each object has certain properties that are used depending on shape type.
+    
+    Each object has a class  representing a shape it could possibly be
+    ex:
+        Object{
+            public:
+                //overloaded constructors for each object type
+
+                //circle
+                Object(std::string objType, std::pair<float,float> newPosition, float newRadius, std::pair<float,float> newVelocity = 0, 
+                                std::pair<float,float> newAcceleration = 0, float newMass = 1) 
+
+                                : objType(newObjType), objectCircle(new Circle(newPosition,newRadius), volicity(newVelocity), acceleration(newAcceleration), mass(newMass)){};
+
+                //rectangle constructor
+                Object(std::string newObjType, Plane newCorners, std::pair<float,float> newVelocity, std::pair<float,float> newAcceleration, float newMass)
+                                : objType(newObjType), objectRectangle(new Rectangle(newCorners), velocity(newVelocity), acceleration(newAcceleration), mass(newMass)){};
+
+                //triangle constructor
+                    ---------------------Not sure on triangles yet ----------------
+                //destructor
+                ~Object{
+                    delete objectCircle
+                    delete objectRectangle
+                    delete ObjectTriangle
+                }
+                
+                //getters and setters
+
+            private:
+                std::string type
+
+                float mass
+                std::pair<float,float> accelleration
+                std::pair<float,float> velocity
+        
+                bool affectedByObjects
+                bool affectedByGravity
+
+                Circle *objectCircle = nullptr;
+                Rectangle *objectRectangle = nullptr;
+                Triangle *objectTriangle = nullptr;
+        }
+        
+---------------Example circle class ------------------
+    Circle{
+        public:
+            -------------- constructor --------------
+            Circle(std::pair<float,float> newCenter, float newradius) : radius(newRadius), center(newCenter){}
+
+            ------------ getters --------------
+            float getRadius(){}
+            std::pair<float,float> getCenter(){}
+
+            float getDiameter(){return radius*2}
+            ------------ setters -------------
+            void setradius
+        private:
+            //circle attributes
+            float radius
+            std::pair<float,float> center
+    }
+--------------- Example rectangle class ----------------
+    Rectangle{
+        public:
+            ------------constructor----------------
+            Rectangle(Plane newCorners) : plane(newCorners){}
+
+            ----------- getters --------------------
+            std::pair<float,float> getLength(){return plane.first.first - plane.second.first}
+            sdt::pair<float,float> getHeight(){return plane.first.second - plane.second.first}
+
+            float getXCenter(){return (plane.first.first + plane.second.first)/2}
+            float getXCenter(){return (plane.first.second + plane.second.second)/2}
+
+            std::pair<float,float> getCenter(){return {getXCenter(),getYCenter()}
+
+            // ------------ setters -------------
+            void changeLeftX(){}
+            void changeRightX(){}
+            void changeTopY(){}
+            void changeBottomY(){}
+
+            void move(){}
+            
+            void setMass();
+            void setVelocity();
+            void setAcceleration();
+
+        private:
+            // ------ rectangle attributes -------------
+            std::pair<std::pair<float,float>,std::pair<float,float>> plane = {bottonLeftCorner,topRightCorner}
+    }
+
+        
+
+        }
+*/
+
+
+
 /**
  * @class ObjectManager
  * @brief Manages game objects, including creation, storage, and deletion.
@@ -43,7 +150,8 @@ public:
     Object *getObjectByName(std::string objName);
     
     /**
-     * @brief Creates object of given object constructor parameters
+     * @brief Creates object of given object constructor parameters.
+     * @param objName name of object to create.
      */
     void addObject(std::string objName);
 
